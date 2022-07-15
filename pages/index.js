d97e1cf36208fbee32fbe982765b78cc81821ed7
codenamerush <aarushisharda@gmail.com>
2022-07-15 20:46:55 +0530

    links

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
-// import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-// export default function Home() {
-//   return (
-//     <div className={styles.container}>
-//       <h1> Hello Next World!</h1>
-//     </div>
-//   )
-// }
+
 function Home() {
-  return <h1>Home Page!</h1>
+  return (
+    <div>
+      <h1>Home Page!</h1>
+      <Link href='/blog'>
+        <a>Blog</a>
+      </Link> 
+      <br></br>
+      <Link href='/product'>
+        <a>Product</a>
+      </Link>  
+    </div>
+  )
 }
 
 export default Home
